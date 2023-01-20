7e70cce1751e4889fa5b452d851aad704a97bc5d
NavindaFernando <m.navindafernando@gmail.com>
2023-01-20 09:15:22 -0800

    Finished ui

---
+++ app/src/main/java/com/example/newapp/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.newapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+}
